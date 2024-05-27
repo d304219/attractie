e34bcc35c56bf1675a9b9b9202df9b88c5f64dfb
d304219 <145758870+d304219@users.noreply.github.com>
2024-05-27 13:24:20 +0200

    Add files via upload

---
+++ attractiepagina/admin/attracties/index.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+require_once '../backend/config.php';
+if(!isset($_SESSION['user_id']))
+{
+    $msg = "Je moet eerst inloggen!";
+    header("Location: $base_url/admin/login.php?msg=$msg");
+    exit;
+}
+?>
+
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>Attractiepagina / Admin</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400;600;700&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
+    <link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
+    <link rel="icon" href="<?php echo $base_url; ?>/favicon.ico" type="image/x-icon" />
+
+    
+
+
+</head>
+
+<body>
+
+    <?php require_once '../../header.php'; ?>
+    <div class="container">
+
+        <a href="create.php">Nieuwe attractie maken &gt;</a>
+
+        <?php
+        require_once '../backend/conn.php';
+        $query = "SELECT * FROM rides";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        $rides = $statement->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+
+        <div class="card-container">
+                
+            <?php foreach($rides as $ride): ?>
+                <?php if($ride['fast_pass'] == "0"): ?>
+                    <div class="card">
+                        <div class="card-img">
+                            <img src="<?php echo $base_url; ?>/img/attracties/<?php echo $ride['img_file']; ?>" alt="<?php echo $ride['title']; ?>">
+                        </div>
+                        <div class="card-info">
+                            <div class="card-info-title">
+                                <h4><span style="text-transform:uppercase;"><?php echo $ride['themeland']; ?></span></h4>
+                                <h3><?php echo $ride['title']; ?></h3>
+                            </div>                    
+
+                            <p><?php echo $ride['description']?></p>
+                            <?php if($ride['min_length'] != 0): ?>
+                                <p class="length"><span style="font-weight: 400"><?php echo $ride['min_length']; ?>cm</span> minimale lengte</p>
+                            <?php else:?>
+                                <p class="length">Geen lengtebeperking</p>
+                            <?php endif; ?>
+                            <!-- <a href="edit.php?id=<?php echo $ride['id']; ?>">Aanpassen</a> -->
+                        </div>
+                    </div>
+                    <?php else: ?>
+                        <div class="card fastpass">
+                        <div class="card-img">
+                            <img src="<?php echo $base_url; ?>/img/attracties/<?php echo $ride['img_file']; ?>" alt="<?php echo $ride['title']; ?>">
+                        </div>
+                        <div class="card-info">
+                            <div class="card-info-title">
+                                <h4><span style="text-transform:uppercase;"><?php echo $ride['themeland']; ?></span></h4>
+                                <h3><?php echo $ride['title']; ?></h3>
+                            </div>                    
+
+                            <p><?php echo $ride['description']?></p>
+                            <?php if($ride['min_length'] != 0): ?>
+                                <p class="length"><span style="font-weight: 400"><?php echo $ride['min_length']; ?>cm</span> minimale lengte</p>
+                            <?php else:?>
+                                <p class="length">Geen lengtebeperking</p>
+                            <?php endif; ?>
+                            <!-- <a href="edit.php?id=<?php echo $ride['id']; ?>">Aanpassen</a> -->
+                        </div>
+                    </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
