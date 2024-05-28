8cf621641334ff201b9cc991e4f06bd5fe227149
Loukman Bouhtala <B.loukman2004@gmail.com>
2024-05-28 15:14:02 +0200

    login en registreren af alleen nog een kleine stukje css

--- admin/register.php
+++ admin/register.php
@@ -1,28 +1,54 @@
-<!DOCTYPE html>
+<?php
+session_start();
+require_once 'backend/config.php';
+?>
+
+<!doctype html>
 <html lang="nl">
-<?php require_once "../head.php";?>
-    <title>Registreren</title>
+
+    <?php require_once "../head.php";?>
+    <title>Attractiepagina / Admin</title>
+
 <body>
-    <?php require_once '../header.php' ?>
-    <div class="wrapper">
-        <form action="backend/registerController.php" method="POST">
+    <?php require_once '../header.php'?>
+        <div class="wrapper">
+
+        <h1>Register</h1> <!-- Hier staat nu de h1 -->
+            <form action="backend/registerController.php" method="POSt">
+
             <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" name="username" id="username" required>
+                <label for="username">Gebruikersnaam</label>
+                <input type="text"name="username"id="username"> 
             </div>
+
             <div class="form-group">
                 <label for="password">Wachtwoord</label>
-                <input type="password" name="password" id="password" required>
+                <input type="password"name="password"id="password">
             </div>
             <div class="form-group">
                 <label for="verifyPassword">Bevestig Wachtwoord</label>
                 <input type="password" name="verifyPassword" id="verifyPassword" required>
             </div>
-            <input class="submit" type="submit" value="Registreren">
-        </form>
-    </div>
+            <input class= "submit" type="submit"value="Login">
+
+            </form>
+        </div>
 </body>
 <style>
+
+    .wrapper h1 {
+        position: relative;
+        z-index: 1;
+        background: #FFFFFF;
+        max-width: 400px;
+        margin: 0 auto 100px;
+        margin-top:20px;
+        padding: 45px;
+        text-align: center;
+        border-radius: 15px;
+        box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    }
+
     form {
         position: relative;
         z-index: 1;
